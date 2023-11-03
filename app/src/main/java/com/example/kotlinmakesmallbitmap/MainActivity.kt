9d5d91e056beab1ce09695fa5871225b4fad17ad
İsmail Vasip <vasip.343@gmail.com>
2023-11-03 12:23:10 +0300

    Recording image to SQLite server

--- app/src/main/java/com/example/kotlinmakesmallbitmap/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmakesmallbitmap/MainActivity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import com.example.kotlinmakesmallbitmap.databinding.ActivityMainBinding
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding :ActivityMainBinding
+    private lateinit var smallBitmap : Bitmap
+    private var selectedBitmap : Bitmap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +19,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    fun imageClicked(view : View){
+    fun saveClicked(view:View){
+        //Recording image to SQLite server
+        if(selectedBitmap != null){
+            smallBitmap = makeSmallerBitmap(selectedBitmap!!,300)
 
+            val outputStream = ByteArrayOutputStream()
+            smallBitmap.compress(Bitmap.CompressFormat.PNG,50,outputStream)
+            val byteArray = outputStream.toByteArray()
+
+            try{
+                val database = this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+                database.execSQL("CREATE TABLE IF NOT EXISTS arts (id INTEGER PRIMARY KEY,image BLOB)")
+
+                val sqlString= "INSERT INTO arts (image) VALUES(?)"
+                val statement = database.compileStatement(sqlString)
+                statement.bindBlob(1,byteArray)
+                statement.execute()
+
+            } catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+
+    fun imageClicked(view : View){
     }
 
     private fun makeSmallerBitmap(image:Bitmap,maxSize :Int) : Bitmap {
